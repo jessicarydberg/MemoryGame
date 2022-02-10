9aaedcd71eb45fc667d4a1c85b1ffefeb5a91dad
jessicarydberg <jessica.rydberg@hotmail.se>
2022-02-10 15:20:33 +0000

    Add basic structure to js document

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function() {
+    
+});
+
+function playGame() {
+    
+};
+
+function openCard() {
+    
+};
+
+function checkPair() {
+
+};
+
+function addMove() {
+
+};
+
+function addPair() {
+
+};
+
+recordCount() {
+
+};
