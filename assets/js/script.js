d8b7a350dd2851dbefc17c19061bf03e98992adb
jessicarydberg <jessica.rydberg@hotmail.se>
2022-02-10 18:54:38 +0000

    Add eventlistener to button, call playgame function and display playcards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,41 @@
 document.addEventListener("DOMContentLoaded", function() {
-    
+    let button = document.getElementsByTagName("button")
+    button[0].addEventListener("click", function() {
+        console.log(elements)
+        playGame();
+    });
+
+    playGame();
+
 });
 
 function playGame() {
+
+    //Create an array of 20 random unique numbers
+    let arrayRandom = []
+    while (arrayRandom.length < 20) {
+        let number = Math.floor(Math.random()*20);
+        if (arrayRandom.includes(number) === false) {
+            arrayRandom.push(number);
+        }
+    }
+
+    //With help of random array,
+    //Create an array of the images in random order.
+    let card = document.getElementsByClassName("card");
+    let shuffledCards = []
+    for(let i = 0; i < 20; i++) {
+        shuffledCards.push(card[arrayRandom[i]])
+    }
     
+    for (let i = 0; i < shuffledCards.length; i++) {
+        let source = shuffledCards[i].getAttribute("src");
+        let img = document.createElement("img");
+        img.setAttribute("src", source);
+        img.className = "card";
+        let cards = document.getElementsByClassName('cards')
+        cards[0].appendChild(img);
+    };
 };
 
 function openCard() {
